d9e4ff926292a8359eee1613b5d8142bb7140976
Felipe Kesley <codebrcursos@gmail.com>
2024-07-11 16:48:37 -0300

    feat: aula 03 - favicon e metadata

--- src/app/(default)/(home)/page.tsx
+++ src/app/(default)/(home)/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>home page</h1>
